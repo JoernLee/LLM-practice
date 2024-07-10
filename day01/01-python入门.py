436edec1e1fd46c26e2673c9d22e89ed77aabdd5
Joern <924901972@qq.com>
2024-07-10 21:52:25 +0800

    day01-parctice

--- day01/01-python入门.py
+++ day01/01-python入门.py
@@ -1,2 +1,21 @@
 # 这是注释
+"""
+这是
+多行
+注释
+"""
 print('Hello World')
+
+# 定义变量 - 基本类型
+name = '乔恩'
+age = 23
+height = 1.8
+address = '北京市昌平区'
+isTrue = True
+# 打印变量
+print(name, age, address)
+
+
+# 输入
+user_input = input('请输入：')
+print(user_input)
