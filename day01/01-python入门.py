1f3cd0506ff27dbce2741943e0ca858451fa8fd0
Joern <924901972@qq.com>
2024-07-24 22:10:44 +0800

    两种结构

--- day01/01-python入门.py
+++ day01/01-python入门.py
@@ -19,3 +19,23 @@ print(name, age, address)
 # 输入
 user_input = input('请输入：')
 print(user_input)
+
+# 条件结构
+age = int(input('请输入你的年龄：'))
+
+if age >= 18:
+    print('已成年')
+else:
+    print('未成年！')
+
+print('-' * 40)
+
+# 循环结构
+for i in range(6):
+    print(i)
+
+for i in range(1,6):
+    if i == 3:
+        print('吃饱了')
+        break
+    print(f'正在吃{i}个苹果')
